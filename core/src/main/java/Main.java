fc23cc9995a86cfc3d6a307ae583b241844c60d4
Luka Kik <luka.kik@iths.se>
2021-06-02 23:44:37 +0200

    Initial commit

---
+++ core/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Utils utils = new Utils();
+        System.out.println("Djes ba");
+        System.out.println(utils.poruka());
+    }
+}
---
+++ utils/src/main/java/Utils.java
@@ -0,0 +1,6 @@
+public class Utils {
+    public String poruka() {
+        System.out.println();
+        return "Djes mala krme te ubolo";
+    }
+}
